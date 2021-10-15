91fd35215d2f69b493bf6e454191ad82a92d3c69
Gerardo Romero <gromero@folcode.com>
2021-10-14 23:37:45 -0300

    added comments

--- db/connection.ts
+++ db/connection.ts
@@ -1,18 +1,18 @@
 import { Sequelize } from 'sequelize';
 
 const db = new Sequelize(
-  'ejemplo_dto', 
-  'root', 
-  '',
+  'ejemplo_dto',  // DB_NAME
+  'root',         // DB_USER
+  '',             // DB_PASSWORD
   {
     host: 'localhost',
     dialect: 'mysql',
     logging: true,   //--cada comando que impaca en la base de datos lo ves por consola
     dialectOptions: {
-        dateStrings: true,
-        typeCast: true
+      dateStrings: true,
+      typeCast: true
     },
-    timezone: '-03:00' // UTC -3 para Argentina
+    // timezone: '-03:00' // UTC -3 para Argentina
   }
 );
 
